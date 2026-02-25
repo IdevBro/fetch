2c0d6c83d651d1c493b2ba4529667f88c82ab47b
mirfazildev <mirfazlmirsharipov@gmail.com>
2026-02-25 16:31:01 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
-import { useState, useEffect } from "react";
-
 function App() {
-  const [info, setInfo] = useState([]);
-
-  useEffect(() => {
-    const URL = "https://jsonplaceholder.typicode.com/users";
-
-    fetch(URL)
-      .then((res) => res.json())
-      .then((data) => setInfo(data))
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
   return (
     <div>
-      {info.map((user) => (
-        <h1 key={user.id}>{user.name}</h1>
-      ))}
+      <h1>salom</h1>
     </div>
   );
 }
---
+++ src/components/Btn.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Btn({ salom }) {
+  return (
+    <div>
+      <button>{salom}</button>
+    </div>
+  );
+}
+
+export default Btn;
